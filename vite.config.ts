7be8d7389ebba266d38a21327463c967e8527a9b
fenglir002-lang <fenglir002@gmail.com>
2026-01-04 09:34:00 +0800

    Configure for GitHub Pages default domain (fenglir002-lang.github.io/wsmdemo)

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-// 注意：必须是 /wsmdemo/，前后都要有斜杠
-base: '/',
+  // 使用 GitHub Pages 默认地址：/wsmdemo/
+  base: '/wsmdemo/',
 })
