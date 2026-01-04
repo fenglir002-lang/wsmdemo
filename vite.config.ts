4045b4025876599d251e6835707df907464ac4c7
fenglir002-lang <fenglir002@gmail.com>
2026-01-04 09:36:32 +0800

    Update base path for new repository wsmdemo-public

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 使用 GitHub Pages 默认地址：/wsmdemo/
-  base: '/wsmdemo/',
+  // 使用 GitHub Pages 默认地址：/wsmdemo-public/
+  base: '/wsmdemo-public/',
 })
